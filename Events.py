acfda242cf54b6fa4ca6c94685bc00e1ab945b5c
Intzider <lukamcm@gmail.com>
2023-03-17 13:14:01 +0100

    rewrite * add bot class, add events class * remove unnecessary files * add streams.json

---
+++ Events.py
@@ -0,0 +1,30 @@
+from discord import utils
+from discord.ext import commands
+
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Music Bot Ready')
+
+    @commands.Cog.listener()
+    async def on_guild_available(self, guild):
+        channel = utils.getchannel = utils.get(guild.text_channels, name='bot-channel')
+        if channel is None:
+            await guild.create_text_channel('bot-channel')
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        voice_state = member.guild.voice_client
+        if voice_state is None:
+            return
+
+        if len(voice_state.channel.members) == 1:
+            if self.bot.voice is not None:
+                self.bot.voice.stop()
+                self.bot.voice = None
+                self.bot.current_station = None
+            await voice_state.disconnect()
